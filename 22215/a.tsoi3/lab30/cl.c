73f4dba912dbb3c52deeec1c66c624a8ff4568ea
Anton Tsoy <a.tsoi3@g.nsu.ru>
2023-12-04 10:09:20 +0700

    Lab 30

--- 22215/a.tsoi3/lab30/cl.c
+++ 22215/a.tsoi3/lab30/cl.c
@@ -27,21 +27,33 @@ int main(int argc, char *argv[]) {
 
     char buf[100];
     memset(buf, 0, sizeof(buf));
-    int rc = read(STDIN_FILENO, buf, sizeof(buf));
-    while(rc > 0) {
-        int written_bytes = write(fd, buf, rc);
-        if(written_bytes == -1){
-            perror("Error while writting");
+    int read_chars = 0;
+    int written_bytes;
+    int curr_write;
+    int need_write;
+    do {
+        read_chars = read(STDIN_FILENO, buf, sizeof(buf));
+        if (read_chars == -1) {
+            perror("Error while reading line from user");
             return -1;
         }
-        while(written_bytes < rc) {
-            rc -= written_bytes;
-            written_bytes += write(fd, buf+written_bytes, rc);
-        }
 
-        rc = read(STDIN_FILENO, buf, sizeof(buf));
-    }
+        written_bytes = 0;
+        curr_write = 0;
+        do {
+            need_write = read_chars - written_bytes;
+            printf("Read: %d, Need: %d\n", read_chars, need_write);
+            curr_write = write(fd, buf + written_bytes, need_write);
+            if (curr_write == -1) {
+                perror("ERROR WRITE ERROR");
+                return -1;
+            }
+            written_bytes += curr_write;
+            printf("Curr: %d, Left: %d\n", curr_write, written_bytes);
+        } while (written_bytes < read_chars);
+
+    } while (read_chars > 0);
+
     close(fd);
-
     return 0;
 }
